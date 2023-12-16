b39c270d31c908db8c3b105f5a99b743fe7aa498
MACRODAT <macrodatservices@gmail.com>
2023-12-16 01:22:59 +0100

    Changes to the file

---
+++ 0-bubble_sort.c
@@ -0,0 +1,34 @@
+#include "sort.h"
+
+/**
+ * _swap - element swapper
+ * @i: first
+ * @j: second
+ * @array: arr
+*/
+void _swap(int *array, int i, int j)
+{
+	int tmp = array[i];
+
+	array[i] = array[j];
+	array[j] = tmp;
+}
+
+/**
+ * bubble_sort - sorting algorithm
+ * @array: the array to sort
+ * @size: size of the array
+*/
+void bubble_sort(int *array, size_t size)
+{
+	size_t i = 0, j = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size - i - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+				_swap(array, j, j + 1);
+		}
+	}
+}
