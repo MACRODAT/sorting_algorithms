921d0c6ebcc0b9b7a1cb86ef4459015dca357b8f
MACRODAT <macrodatservices@gmail.com>
2023-12-16 11:02:58 +0100

    Changes to the file

--- sort.h
+++ sort.h
@@ -23,5 +23,6 @@ void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void _swap(int *array, int i, int j);
+void selection_sort(int *array, size_t size);
 
 #endif
