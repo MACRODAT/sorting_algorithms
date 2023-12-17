8720ba6cc911d48b909c4ffd08f8128ef30296da
MACRODAT <macrodatservices@gmail.com>
2023-12-18 00:54:09 +0100

    Changes to the file

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -6,29 +6,51 @@
  * @array: list
  * @size: size
 */
+#include "sort.h"
+
+/**
+ * get_max_gap - gets the a largest Knuth Sequence gap for this size
+ * @size: the size of the array
+ *
+ * Return: the gap size
+ */
+size_t get_max_gap(size_t size)
+{
+	size_t n;
+
+	n = 1;
+	while (n < size)
+		n = n * 3 + 1;
+	return ((n - 1) / 3);
+}
+
+/**
+ * shell_sort - shell_sort
+ * @array: the integer array to sort
+ * @size: the size of the array
+ *
+ * Return: void
+ */
 void shell_sort(int *array, size_t size)
 {
-	size_t i = 0, j = 0, k;
+	size_t gap, i, j;
+	int temp;
 
-	if (!array || !*array || !size)
+	if (!array || !size)
 		return;
-	k = 1, i = size / 2;
-	while ((j = k * 3 + 1) < i)
-		k = j;
-	i = j = 0;
-	while (k > 0)
+
+
+	for (gap = get_max_gap(size); gap; gap = (gap - 1) / 3)
 	{
-		for (i = size - 1; i - k < size; i--)
+		for (i = gap; i < size; i++)
 		{
-			j = 1;
-			while (i >= k * j)
+			temp = array[i];
+			for (j = i; j > gap - 1 && array[j - gap] > temp; j -= gap)
 			{
-				if (array[i] < array[i - k * j])
-					_swap(array, i, i - j * k);
-				j++;
+				array[j] = array[j - gap];
 			}
+			array[j] = temp;
 		}
-		k = (k - 1) / 3;
 		print_array(array, size);
 	}
 }
