85651e85b484b98e192c1cb0700e38f23d517284
MACRODAT <macrodatservices@gmail.com>
2023-12-20 01:46:15 +0100

    Changes to the file

--- 104-main.c
+++ 104-main.c
@@ -11,7 +11,7 @@
  */
 int main(void)
 {
-    int array[] = {19, 48, 99, 71, 13, 52, 96, 73, 86, 7};
+    int array[] = {19, 48, 99, 71, 13, 52, 96, 73, 86, 7, 12, 11, 1, 1000};
     size_t n = sizeof(array) / sizeof(array[0]);
 
     print_array(array, n);
